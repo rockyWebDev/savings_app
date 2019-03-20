9cec2f5b605d533968c046fc8e7ed9cca5cde72a
rockyWebDev <rocky.webdev@gmail.com>
2019-03-20 23:11:10 +0000

    Added a string that stating whether it will take weeks or months

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ function calculate(initial=0){
     initial=document.getElementById("initialSum").value;
     sum=document.getElementById("savingSum").value;
     total=document.getElementById("endSum").value;
+    let time
+    document.getElementById("week").checked?time="weeks":time="months";
     result=Math.ceil((total-initial)/sum);
-    console.log(Math.ceil(result));
-    return document.getElementById("result").innerHTML="it will take you "+result;
+    return document.getElementById("result").innerHTML="it will take you "+result+" "+time;
 }
