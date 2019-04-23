4680b2ab23005acc326cfdbb88f509975065886c
rockyWebDev <rocky.webdev@gmail.com>
2019-04-23 22:47:32 +0100

    changed the formatting of the returned values

--- js/script.js
+++ js/script.js
@@ -7,14 +7,10 @@ el.addEventListener("click", calculate, false);
 function calculate(initial=0){
     initial=document.getElementById("initialSum").value;
     sum=document.getElementById("savingSum").value;
-    total=document.getElementById("endSum").value;
-    let time
-    document.getElementById("week").checked?time="weeks":time="months";
-    result=Math.ceil((total-initial)/sum);
-    if(time=="weeks"&&result>=4&&result%4==0){
-        time="months";
-         result= result/4;
-    }
-    
-    return document.getElementById("result").innerHTML="it will take you "+result+" "+time;
+    total=document.getElementById("endSum").value
+    document.getElementById("week").checked?result=Math.ceil((total-initial)/sum):result=(Math.ceil((total-initial)/sum))*4;
+    if (result<4){ 
+        return document.getElementById("result").innerHTML="it will take you "+result+" weeks";}
+    else if (result>=4){
+        return document.getElementById("result").innerHTML="it will take you "+(result/4)+" months";}
 }
