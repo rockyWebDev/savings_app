68caf0dd05c2aaf00688691b594711df9ad7a4c1
rockyWebDev <rocky.webdev@gmail.com>
2019-04-24 22:38:38 +0100

    ammending the result values

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@ let el= document.getElementById("calculate");
 let initial;
 let sum;
 let total;
-let result
+let result;
 el.addEventListener("click", calculate, false);
 function calculate(initial=0){
     initial=document.getElementById("initialSum").value;
     sum=document.getElementById("savingSum").value;
     total=document.getElementById("endSum").value
     document.getElementById("week").checked?result=Math.ceil((total-initial)/sum):result=(Math.ceil((total-initial)/sum))*4;
-    if (result<4){ 
-        return document.getElementById("result").innerHTML="it will take you "+result+" weeks";}
-    else if (result>=4){
-        return document.getElementById("result").innerHTML="it will take you "+(result/4)+" months";}
-}
+    if (result>=53&&result%53!=0&& (result%53)%4==0){
+        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years and"+(result%53)+"and weeks";}
+    else if (result>=53&&result%53!=0){
+        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years"+(result%53)+" months and "+((result%53)%4)+" weeks";}
+    else if (result>=53&&result%53==0){
+        return document.getElementById("result").innerHTML="It will take you "+(result/53)+" years";}
+    else if (result>=4 && result%4!=0){
+        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/4)+" months and "+(result%4)+" weeks";}
+    else if (result>=4 && result%4==0){
+        return document.getElementById("result").innerHTML="It will take you "+(result/4)+" months";}
+    else if (result<4){ 
+        return document.getElementById("result").innerHTML="It will take you "+result+" weeks";}
+    
+    }     
