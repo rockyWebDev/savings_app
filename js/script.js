b8f0778b323832adc0e01558549b6b5984ce497c
rockyWebDev <rocky.webdev@gmail.com>
2019-04-28 22:16:20 +0100

    further changes to the result formatting

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ function calculate(initial=0){
     sum=document.getElementById("savingSum").value;
     total=document.getElementById("endSum").value
     document.getElementById("week").checked?result=Math.ceil((total-initial)/sum):result=(Math.ceil((total-initial)/sum))*4;
-    if (result>=53&&result%53!=0&& (result%53)%4==0){
-        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years and"+(result%53)+"and weeks";}
-    else if (result>=53&&result%53!=0){
-        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years"+(result%53)+" months and "+((result%53)%4)+" weeks";}
+    if (result>53&&result%53!=0&& (result%53)%4!=0){
+        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years "+parseInt((result%53)/4)+" months and "+parseInt(((result%53)/4))%4+" weeks";}
+    else if (result>53&&result%53!=0&& (result%53)<4){
+        return document.getElementById("result").innerHTML="It will take you "+parseInt(result/53)+" years and"+(result%53)+" weeks";}
     else if (result>=53&&result%53==0){
         return document.getElementById("result").innerHTML="It will take you "+(result/53)+" years";}
     else if (result>=4 && result%4!=0){
